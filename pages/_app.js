9e828c89540dc1c7b68dfa04b96ddbb23f437f96
derevi <@derevi>
2023-03-16 00:15:21 -0400

    basic layout created, using template from mui docs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,47 @@
-import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import SideBar from '@/components/sidebar/side-bar';
+
+
+
+export default function  App({ Component, pageProps, currentUser }){
+  //Component: the component we are trying to load from one of the pages. It is the actual page we want to show the user
+  //pageProps: the set of components that we intended to pass to the (1st arg) "Component"
+
+  return (<div>
+      {/* <Header  currentUser={currentUser} /> */}
+
+      <SideBar>
+        <Component currentUser={currentUser} {...pageProps}/>
+      </SideBar>
+      {/* <ContentBox >
+          <Component currentUser={currentUser} {...pageProps}/>
+      </ContentBox> */}
+  </div>);
 }
+
+// Page.getLayout = (page) => (
+//   <SideBar>
+//     {page}
+//   </SideBar>
+// );
+
+App.getInitialProps = async appContext =>{
+  // Load user data here....
+  const data = {
+    currentUser:{
+        login: "testUser", 
+        pass:"testPass"
+    }
+  }
+  let pageProps={};
+     //loads 'getInitialProps' of (child) page only if it exists
+  if(appContext.Component.getInitialProps){
+      pageProps = await appContext.Component.getInitialProps(appContext.ctx,data.currentUser)
+    
+  }
+  return {
+      pageProps,
+      ...data //current user data
+  };
+ 
+ }
